4272b55eadcea122d109a6cb8391801d8ba18105
TanyaEf <efimenkot@gmail.com>
2020-02-11 17:06:25 +0200

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingListDetailComponent } from './shopping-list-detail/shopping-list-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListDetailComponent
   ],
   imports: [
     BrowserModule
