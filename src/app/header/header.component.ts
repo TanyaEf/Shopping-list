3cdfd573849262be97151b94a0706e88a6a66a5b
TanyaEf <efimenkot@gmail.com>
2020-02-18 17:05:46 +0200

    event binding, switch between components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  // tslint:disable-next-line:no-output-rename
+  @Output('componentSelected') componentSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(componentName: string) {
+  this.componentSelected.emit(componentName);
+  }
 }
