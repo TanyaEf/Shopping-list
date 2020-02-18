b1437c0f0ba13cf23635eb5eff606252600f3172
TanyaEf <efimenkot@gmail.com>
2020-02-18 17:27:05 +0200

    Passing Recipe Data with Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
     // tslint:disable-next-line:max-line-length
     new Recipe('A test recipe', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg'),
     // tslint:disable-next-line:max-line-length
-    new Recipe('A test recipe', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg')
+    new Recipe('A test recipe 2', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg')
   ];
 
   constructor() { }
--- src/app/recipes/recipe-list/recpe-item/recpe-item.component.ts
+++ src/app/recipes/recipe-list/recpe-item/recpe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Recipe} from '../../recipe.model';
 
 @Component({
   selector: 'app-recpe-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recpe-item.component.css']
 })
 export class RecpeItemComponent implements OnInit {
+  @Input('recipe') recipe: Recipe;
 
   constructor() { }
 
